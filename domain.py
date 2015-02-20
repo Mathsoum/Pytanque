ad616313950e518ffab30b55e20474dbeed729ac
Mathieu SOUM <mathieu.soum@gmail.com>
2015-02-20 10:08:58 +0100

    Match in domain. str.format() instead of '%'.

--- domain.py
+++ domain.py
@@ -7,4 +7,13 @@ class Team():
         self.club = club
 
     def __str__(self):
-        return '%s (%s)' % (self.name, self.club)
+        return '{0} ({1})'.format(self.name, self.club)
+
+
+class Match():
+    def __init__(self, first_team, second_team):
+        self.first_team = first_team
+        self.second_team = second_team
+
+    def __str__(self):
+        return '{0} vs {1}'.format(self.first_team, self.second_team)
