07d9d2198e825f2c0fb06589f1936bd0f870ac1b
Mathieu SOUM <mathieu.soum@gmail.com>
2015-02-20 09:58:30 +0100

    Slots for winner selector management.

--- contestwidget.py
+++ contestwidget.py
@@ -45,25 +45,48 @@ class ContestWidget(QWidget):
 
         # Winning team selector
         first_team_button = QRadioButton("Team #1")
+        first_team_button.toggled.connect(self.winner_selected_slot)
         second_team_button = QRadioButton("Team #2")
+        second_team_button.toggled.connect(self.winner_selected_slot)
+
+        # Selectors in button group for better management
         self.button_group = QButtonGroup()
         self.button_group.addButton(first_team_button)
         self.button_group.addButton(second_team_button)
-        team_button_layout = QHBoxLayout()
-        team_button_layout.addStretch(1)
-        team_button_layout.addWidget(first_team_button)
-        team_button_layout.addWidget(second_team_button)
-        team_button_layout.addStretch(1)
-        validate_button = QPushButton("Validate")
+
+        # Winner selector layout
+        winner_selector_layout = QHBoxLayout()
+        winner_selector_layout.addStretch(1)
+        winner_selector_layout.addWidget(first_team_button)
+        winner_selector_layout.addWidget(second_team_button)
+        winner_selector_layout.addStretch(1)
+
+        # Validate button
+        self.validate_button = QPushButton("Validate")
+        self.validate_button.setEnabled(False)
+        self.validate_button.clicked.connect(self.clear_selected_winner)  # FIXME For test purposes only
+
+        # Validate button layout
         validate_button_layout = QHBoxLayout()
         validate_button_layout.addStretch(1)
-        validate_button_layout.addWidget(validate_button)
+        validate_button_layout.addWidget(self.validate_button)
         validate_button_layout.addStretch(1)
 
+        # Main layout
         main_layout = QVBoxLayout()
         main_layout.addLayout(match_layout)
-        main_layout.addLayout(team_button_layout)
+        main_layout.addLayout(winner_selector_layout)
         main_layout.addLayout(validate_button_layout)
 
         self.setLayout(main_layout)
 
+    def winner_selected_slot(self):
+        # Enable/Disable validate button
+        self.validate_button.setEnabled(self.button_group.checkedButton() is not None)
+
+    def clear_selected_winner(self):
+        self.button_group.setExclusive(False)
+        for button in self.button_group.buttons():
+            button.setChecked(False)
+
+        self.button_group.setExclusive(True)
