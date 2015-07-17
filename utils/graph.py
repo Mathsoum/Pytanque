b18c4f4ccf7a67954a4b07b904a97fb6f46a41a3
Mathieu SOUM <mathieu.soum@gmail.com>
2015-07-17 18:52:08 +0200

    Primary steps on graph creation.

--- utils/graph.py
+++ utils/graph.py
@@ -16,3 +16,25 @@ class ReverseBinaryGraph:
 
     def add_leave(self, data):
         self.leaves.append(data)
+        if len(self.leaves) == 2:
+            left = self.leaves[0]
+            right = self.leaves[1]
+            self.set_parent(left, right)
+        elif len(self.leaves) == 3:
+            left = self.leaves[0].parent
+            right = self.leaves[2]
+            self.set_parent(left, right)
+        elif len(self.leaves) == 4:
+            left = self.leaves[2]
+            right = self.leaves[3]
+            self.set_parent(left, right)
+            left = self.leaves[0].parent
+            right = self.leaves[2].parent
+            self.set_parent(left, right, self.leaves[0].parent.parent)
+
+    @staticmethod
+    def set_parent(left, right, parent=Vertex()):
+        left.parent = parent
+        right.parent = parent
+        parent.left = left
+        parent.right = right
--- utils/test_reverseBinaryGraph.py
+++ utils/test_reverseBinaryGraph.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from utils.graph import ReverseBinaryGraph
+from utils.graph import ReverseBinaryGraph, Vertex
 
 __author__ = 'msoum'
 
@@ -12,6 +12,38 @@ class TestReverseBinaryGraph(TestCase):
         self.assertEqual(0, len(self.graph.leaves))
 
     def test_one_leave(self):
-        self.graph.add_leave(0)
-
+        self.graph.add_leave(Vertex())
         self.assertEqual(1, len(self.graph.leaves))
+
+    def test_two_leaves(self):
+        self.graph.add_leave(Vertex())
+        self.graph.add_leave(Vertex())
+        self.assertEqual(2, len(self.graph.leaves))
+        self.assertIsNotNone(self.graph.leaves[0].parent)
+        self.assertIsNotNone(self.graph.leaves[1].parent)
+        self.assertIs(self.graph.leaves[0].parent, self.graph.leaves[1].parent)
+
+    def test_three_leaves(self):
+        self.graph.add_leave(Vertex())
+        self.graph.add_leave(Vertex())
+        self.graph.add_leave(Vertex())
+        self.assertEqual(3, len(self.graph.leaves))
+        self.assertIsNotNone(self.graph.leaves[0].parent)
+        self.assertIsNotNone(self.graph.leaves[1].parent)
+        self.assertIsNotNone(self.graph.leaves[2].parent)
+        self.assertIs(self.graph.leaves[0].parent, self.graph.leaves[1].parent)
+        self.assertIs(self.graph.leaves[0].parent.parent, self.graph.leaves[2].parent)
+
+    def test_four_leaves(self):
+        self.graph.add_leave(Vertex())
+        self.graph.add_leave(Vertex())
+        self.graph.add_leave(Vertex())
+        self.graph.add_leave(Vertex())
+        self.assertEqual(4, len(self.graph.leaves))
+        self.assertIsNotNone(self.graph.leaves[0].parent)
+        self.assertIsNotNone(self.graph.leaves[1].parent)
+        self.assertIsNotNone(self.graph.leaves[2].parent)
+        self.assertIsNotNone(self.graph.leaves[3].parent)
+        self.assertIs(self.graph.leaves[0].parent, self.graph.leaves[1].parent)
+        self.assertIs(self.graph.leaves[2].parent, self.graph.leaves[3].parent)
+        self.assertIs(self.graph.leaves[0].parent.parent, self.graph.leaves[2].parent.parent)
