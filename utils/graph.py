e2109248a0300e6fea140fd388ab97a97caeff95
Mathieu SOUM <mathieu.soum@gmail.com>
2015-07-21 17:33:25 +0200

    Factoring graph creation. Dynamic graph creation.

--- utils/graph.py
+++ utils/graph.py
@@ -10,37 +10,52 @@ class Vertex:
 
 
 class ReverseBinaryGraph:
-    def __init__(self):
-        self.leaves = []
+    def __init__(self, leaves_list):
+        self.leaves = list(leaves_list)
+        self.build_tree(leaves_list)
 
-    def add_leave(self, data):
-        self.leaves.append(Vertex(data))
-        if len(self.leaves) == 2:
-            left = self.leaves[0]
-            right = self.leaves[1]
-            self.set_parent(left, right)
-        elif len(self.leaves) == 3:
-            left = self.leaves[0].parent
-            right = self.leaves[2]
-            self.set_parent(left, right)
-        elif len(self.leaves) == 4:
-            left = self.leaves[2]
-            right = self.leaves[3]
-            self.set_parent(left, right)
-            left = self.leaves[0].parent
-            right = self.leaves[2].parent
-            self.set_parent(left, right, self.leaves[0].parent.parent)
-        elif len(self.leaves) == 5:
-            left = self.leaves[0].parent
-            right = self.leaves[4]
-            self.set_parent(left, right)
-            left = self.leaves[4].parent
-            right = self.leaves[2].parent
-            self.set_parent(left, right, self.leaves[2].parent.parent)
+    def build_tree(self, leaves_list):
+        if len(leaves_list) == 0:
+            return Vertex()
+        if len(leaves_list) == 1:  # End of recursion
+            leave = Vertex(leaves_list[0])
+            idx = self.leaves.index(leaves_list[0])
+            self.leaves[idx] = leave
+            return leave  # Return a simple vertex as sub-root
+        else:  # Recursion for build sub-trees TODO Sub-trees construction could be computed in parallel
+            left_list, right_list = self.split_leave_list(leaves_list)
+            left_root = self.build_tree(left_list)
+            right_root = self.build_tree(right_list)
+            return self.create_parent(left_root, right_root) #  Return freshly build tree
+
+    def split_leave_list(self, leaves_list):
+        power, rest = self.power_two_decomposition(len(leaves_list))
+        if rest <= power // 2:
+            left_list = leaves_list[:(power // 2) + rest]
+            right_list = leaves_list[(power // 2) + rest:]
+        else:
+            left_list = leaves_list[:power]
+            right_list = leaves_list[power:]
+
+        return left_list, right_list
 
     @staticmethod
-    def set_parent(left, right, parent=Vertex()):
-        left.parent = parent
-        right.parent = parent
-        parent.left = left
-        parent.right = right
+    def create_parent(left_root, right_root):
+        parent = Vertex()
+        parent.left = left_root
+        parent.right = right_root
+        left_root.parent = right_root.parent = parent
+        return parent
+
+    @staticmethod
+    def power_two_decomposition(number):
+        if number == 1:
+            return 0, 1
+        else:
+            power = 2
+            previous = 2
+            while power <= number:
+                previous = power
+                power *= 2
+
+            return previous, number - previous
--- utils/test_reverse_binary_graph.py
+++ utils/test_reverse_binary_graph.py
@@ -1,68 +1,64 @@
 from unittest import TestCase
 
-from domain.data_structures import Team
 from utils.graph import ReverseBinaryGraph
 
 __author__ = 'msoum'
 
 
 class TestReverseBinaryGraph(TestCase):
-    def setUp(self):
-        self.graph = ReverseBinaryGraph()
-
     def test_creation(self):
+        self.graph = ReverseBinaryGraph([])
         self.assertEqual(0, len(self.graph.leaves))
 
     def test_one_leave(self):
-        self.graph.add_leave(Team('A', 'A'))
+        self.graph = ReverseBinaryGraph([1])
         self.assertEqual(1, len(self.graph.leaves))
 
     def test_two_leaves(self):
-        self.graph.add_leave(Team('A', 'A'))
-        self.graph.add_leave(Team('B', 'B'))
+        self.graph = ReverseBinaryGraph(range(1, 3))
         self.assertEqual(2, len(self.graph.leaves))
-        self.assertIsNotNone(self.graph.leaves[0].parent)
-        self.assertIsNotNone(self.graph.leaves[1].parent)
+        for node in [it.parent for it in self.graph.leaves]:
+            self.assertIsNotNone(node)
+
         self.assertIs(self.graph.leaves[0].parent, self.graph.leaves[1].parent)
 
     def test_three_leaves(self):
-        self.graph.add_leave(Team('A', 'A'))
-        self.graph.add_leave(Team('B', 'B'))
-        self.graph.add_leave(Team('C', 'C'))
+        self.graph = ReverseBinaryGraph(range(1, 4))
         self.assertEqual(3, len(self.graph.leaves))
-        self.assertIsNotNone(self.graph.leaves[0].parent)
-        self.assertIsNotNone(self.graph.leaves[1].parent)
-        self.assertIsNotNone(self.graph.leaves[2].parent)
+        for node in [it.parent for it in self.graph.leaves]:
+            self.assertIsNotNone(node)
+
         self.assertIs(self.graph.leaves[0].parent, self.graph.leaves[1].parent)
         self.assertIs(self.graph.leaves[0].parent.parent, self.graph.leaves[2].parent)
 
     def test_four_leaves(self):
-        self.graph.add_leave(Team('A', 'A'))
-        self.graph.add_leave(Team('B', 'B'))
-        self.graph.add_leave(Team('C', 'C'))
-        self.graph.add_leave(Team('D', 'D'))
+        self.graph = ReverseBinaryGraph(range(1, 5))
         self.assertEqual(4, len(self.graph.leaves))
-        self.assertIsNotNone(self.graph.leaves[0].parent)
-        self.assertIsNotNone(self.graph.leaves[1].parent)
-        self.assertIsNotNone(self.graph.leaves[2].parent)
-        self.assertIsNotNone(self.graph.leaves[3].parent)
+        for node in [it.parent for it in self.graph.leaves]:
+            self.assertIsNotNone(node)
+
         self.assertIs(self.graph.leaves[0].parent, self.graph.leaves[1].parent)
         self.assertIs(self.graph.leaves[2].parent, self.graph.leaves[3].parent)
         self.assertIs(self.graph.leaves[0].parent.parent, self.graph.leaves[2].parent.parent)
 
     def test_five_leaves(self):
-        self.graph.add_leave(Team('A', 'A'))
-        self.graph.add_leave(Team('B', 'B'))
-        self.graph.add_leave(Team('C', 'C'))
-        self.graph.add_leave(Team('D', 'D'))
-        self.graph.add_leave(Team('E', 'E'))
+        self.graph = ReverseBinaryGraph(range(1, 6))
         self.assertEqual(5, len(self.graph.leaves))
-        self.assertIsNotNone(self.graph.leaves[0].parent)
-        self.assertIsNotNone(self.graph.leaves[1].parent)
-        self.assertIsNotNone(self.graph.leaves[2].parent)
-        self.assertIsNotNone(self.graph.leaves[3].parent)
-        self.assertIsNotNone(self.graph.leaves[4].parent)
+        for node in [it.parent for it in self.graph.leaves]:
+            self.assertIsNotNone(node)
+
+        self.assertIs(self.graph.leaves[0].parent, self.graph.leaves[1].parent)
+        self.assertIs(self.graph.leaves[0].parent.parent, self.graph.leaves[2].parent)
+        self.assertIs(self.graph.leaves[3].parent, self.graph.leaves[4].parent)
+        self.assertIs(self.graph.leaves[0].parent.parent.parent, self.graph.leaves[3].parent.parent)
+
+    def test_seven_leaves(self):
+        self.graph = ReverseBinaryGraph(range(1, 8))
+        self.assertEqual(7, len(self.graph.leaves))
+        for node in [it.parent for it in self.graph.leaves]:
+            self.assertIsNotNone(node)
+
         self.assertIs(self.graph.leaves[0].parent, self.graph.leaves[1].parent)
         self.assertIs(self.graph.leaves[2].parent, self.graph.leaves[3].parent)
-        self.assertIs(self.graph.leaves[0].parent.parent, self.graph.leaves[4].parent)
-        self.assertIs(self.graph.leaves[0].parent.parent.parent, self.graph.leaves[4].parent.parent)
+        self.assertIs(self.graph.leaves[4].parent, self.graph.leaves[5].parent)
+        self.assertIs(self.graph.leaves[4].parent.parent, self.graph.leaves[6].parent)
